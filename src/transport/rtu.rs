31e9bc8a0ccb555d87c58cc4ffda38f2abb0f0ad
Ryouhei Kato <r-kato@musen.co.jp>
2025-01-14 15:04:26 +0900

    トランスポート層の実装

--- src/transport/rtu.rs
+++ src/transport/rtu.rs
@@ -1,20 +1,22 @@
 use core::time::Duration;
 
+use crate::{
+    error::ModbusTransportError,
+    frame::{
+        pdu::Pdu,
+        rtu::{Adu, RtuFrameHandler},
+    },
+    lib::*,
+};
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     time::{sleep, Instant},
 };
+pub use tokio_serial::Parity;
 use tokio_serial::{SerialPortBuilder, SerialPortBuilderExt, SerialStream};
 
-use crate::{
-    common::Pdu,
-    error::{ModbusRtuError, ModbusTransportError},
-    interface::Transport,
-};
-
-use super::{Adu, Result};
-
-pub use tokio_serial::Parity;
+use super::Transport;
 
 const RTU_BITS_PER_CHAR: u8 = 11;
 
@@ -66,41 +68,36 @@ impl SerialTransport {
     /// Set the slave address
     ///
     /// Note. 2.2 MODBUS Addressing rules
-    pub fn set_slave_addr(&mut self, slave_addr: u8) -> Result<()> {
-        if (1..=247).contains(&slave_addr) {
-            self.ctx.slave_addr = slave_addr;
-            Ok(())
-        } else {
-            Err(ModbusRtuError::InvalidSlaveAddress(slave_addr).into())
-        }
+    pub fn set_slave_addr(&mut self, slave_addr: u8) {
+        self.ctx.slave_addr = slave_addr;
+        // if (1..=247).contains(&slave_addr) {
+        //     Ok(())
+        // } else {
+        //     Err(ModbusRtuError::InvalidSlaveAddress(slave_addr).into())
+        // }
     }
 }
 
 impl Transport for SerialTransport {
-    async fn send(&mut self, pdu: &Pdu) -> Result<()> {
-        self.buffer.clear();
-
-        // Modbus RTU header
-        self.buffer.put_u8(self.ctx.slave_addr)?;
-        // Protocol Data Unit
-        self.buffer.extend_from_slice(pdu.as_slice())?;
-        // CRC
-        let crc = calc_crc(self.buffer.as_slice());
-        self.buffer.put_u16_le(crc)?;
+    async fn send(
+        &mut self,
+        pdu: &Pdu,
+    ) -> core::result::Result<(), Box<dyn error::Error + Send + Sync>> {
+        RtuFrameHandler::build_frame(&mut self.buffer, self.ctx.slave_addr, pdu)?;
 
         self.port.write_all(self.buffer.as_slice()).await?;
 
         Ok(())
     }
 
-    async fn recv(&mut self) -> Result<Pdu> {
+    async fn recv(&mut self) -> core::result::Result<Pdu, Box<dyn error::Error + Send + Sync>> {
         self.buffer.clear();
         let t3_5_timer = sleep(Duration::from_secs(86400));
         tokio::pin!(t3_5_timer);
 
         loop {
             tokio::select! {
-                res = self.port.read(self.buffer.as_write_slice()) => {
+                res = self.port.read(self.buffer.as_mut()) => {
                     let current_time = Instant::now();
 
                     match res {
@@ -109,27 +106,21 @@ impl Transport for SerialTransport {
                             if !self.buffer.is_empty() {
                                 let elapsed = current_time.duration_since(self.ctx.latest_time);
                                 if elapsed > self.ctx.t1_5 {
-                                    return Err(ModbusRtuError::FrameIncomplete.into());
+                                    return Err(ModbusTransportError::FrameIncomplete.into());
                                 }
                             }
 
-                            self.buffer.set_position(self.buffer.len() + n);
+                            let len = self.buffer.len() + n;
+                            unsafe {self.buffer.set_len(len)};
 
-                            if self.buffer.get(0) == Some(&self.ctx.slave_addr) {
-                                // Validate frame if slave address matches
-                                let len = self.buffer.len();
-                                if len > 2 {
-                                    let crc = self.buffer.get_u16_le(len - 2).unwrap();
-                                    if checksum(&self.buffer.as_slice()[..len - 2], crc).is_ok() {
-                                        return Ok(self.buffer.pdu()?);
-                                    }
-                                }
+                            if let Ok(pdu) = RtuFrameHandler::parse_frame(self.buffer.as_slice(), self.ctx.slave_addr) {
+                                return Ok(pdu);
                             } else {
                                 // Ignore the frame
                                 self.buffer.clear();
                             }
                         }
-                        Err(ref err) if err.kind() == std::io::ErrorKind::TimedOut && self.buffer.is_empty() => {}
+                        // Err(ref err) if err.kind() == std::io::ErrorKind::TimedOut && self.buffer.is_empty() => {}
                         Err(err) => return Err(err.into()),
                     }
 
@@ -138,14 +129,12 @@ impl Transport for SerialTransport {
                     continue;
                 }
                 _ = &mut t3_5_timer => {
-                    let len = self.buffer.len();
-                    if len > 2 {
-                        let crc = self.buffer.get_u16_le(len - 2).unwrap();
-                        checksum(&self.buffer.as_slice()[..len - 2], crc)?;
-                        return Ok(self.buffer.pdu()?);
+                    if let Ok(pdu) = RtuFrameHandler::parse_frame(self.buffer.as_slice(), self.ctx.slave_addr) {
+                        return Ok(pdu);
+                    } else {
+                        return Err(ModbusTransportError::Timeout.into());
                     }
 
-                    return Err(ModbusTransportError::Timeout);
                 }
             }
         }
@@ -205,50 +194,23 @@ impl SerialTransportBuilder {
         }
     }
 
-    pub fn build(self) -> Result<SerialTransport> {
+    pub fn build(self) -> Result<SerialTransport, ModbusTransportError> {
         let port = self
             .inner
             .open_native_async()
-            .map_err(|err| ModbusRtuError::SerialError(err))?;
+            .map_err(|err| ModbusTransportError::TransportError(err.into()))?;
 
         Ok(SerialTransport {
             port,
             ctx: self.ctx,
-            buffer: Adu::new(),
+            buffer: Adu::default(),
         })
-    }
-}
-
-/// Generate a Modbus RTU frame CRC.
-fn calc_crc(data: &[u8]) -> u16 {
-    let mut crc = 0xFFFFu16;
-
-    for byte in data {
-        crc ^= u16::from(*byte);
-
-        for _ in 0..8 {
-            if crc & 0x0001 != 0 {
-                crc = (crc >> 1) ^ 0xA001;
-            } else {
-                crc >>= 1;
-            }
-        }
-    }
-
-    crc
-}
-
-fn checksum(data: &[u8], crc: u16) -> Result<()> {
-    if calc_crc(data) == crc {
-        Ok(())
-    } else {
-        Err(ModbusTransportError::RtuError(ModbusRtuError::CrcValidationFailure).into())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::app::model::{
+    use crate::frame::pdu::function::{
         request::ReadHoldingRegistersRequest, response::ReadHoldingRegistersResponse,
     };
 
@@ -261,7 +223,7 @@ mod tests {
             .build()
             .unwrap();
 
-        transport.set_slave_addr(0x50).unwrap();
+        transport.set_slave_addr(0x50);
 
         for _ in 0..10 {
             let start = Instant::now();
--- src/transport/tcp.rs
+++ src/transport/tcp.rs
@@ -1,1 +1,2 @@
 // ToDo. Modbus TCPの実装を追加する
+const MAX_ADU_SIZE: usize = 260;
